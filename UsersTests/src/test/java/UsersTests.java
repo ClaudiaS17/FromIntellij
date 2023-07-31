4137a0ae23d5225810245ac885111c4e047c1ed2
claudia <claudia_surcel@yahoo.com>
2023-08-01 00:01:14 +0100

    Update

--- UsersTests/src/test/java/UsersTests.java
+++ UsersTests/src/test/java/UsersTests.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
-public class Tests{
+public class UsersTests{
     public static WebDriver driver;
 
 @Before
