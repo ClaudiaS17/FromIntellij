53adea26a25ac3f13f407b80b7c9be2a7ee6913d
claudia <claudia_surcel@yahoo.com>
2023-07-31 23:47:35 +0100

    First Java

--- UsersTests/src/test/java/Tests.java
+++ UsersTests/src/test/java/Tests.java
@@ -1,21 +1,28 @@
+import com.sun.istack.internal.NotNull;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
+public class Tests{
+    public static WebDriver driver;
 
-    public class Tests {
+@Before
+public void before() {
+    WebDriverManager.chromedriver().setup();
+    ChromeOptions options = new ChromeOptions();
+    options.addArguments("--remote-allow-origins=*");
+    WebDriver driver = new ChromeDriver(options);
+    driver.manage().window().maximize();
+}
 
         @Test
         public void test1() {
-            WebDriverManager.chromedriver().setup();
-            ChromeOptions options = new ChromeOptions();
-            options.addArguments("--remote-allow-origins=*");
-            WebDriver driver = new ChromeDriver(options);
-            driver.manage().window().maximize();
-            driver.get("http://solar.schoolnewgen.com/websolar/login");
+
             driver.findElement(By.id("email")).sendKeys("super_admin@gmail.com");
             driver.findElement(By.id("password")).sendKeys("secret");
             driver.findElement(By.xpath("/html/body/div/div[2]/div/form/div[3]/div[2]/button")).click();
@@ -25,12 +32,12 @@ import org.openqa.selenium.chrome.ChromeOptions;
         }
         @Test
         public void test2() {
-            WebDriverManager.chromedriver().setup();
-            ChromeOptions options = new ChromeOptions();
-            options.addArguments("--remote-allow-origins=*");
-            WebDriver driver = new ChromeDriver(options);
-            driver.manage().window().maximize();
-            driver.get("http://solar.schoolnewgen.com/websolar/login");
+            //WebDriverManager.chromedriver().setup();
+            //ChromeOptions options = new ChromeOptions();
+            //options.addArguments("--remote-allow-origins=*");
+            //WebDriver driver = new ChromeDriver(options);
+            //driver.manage().window().maximize();
+            //driver.get("http://solar.schoolnewgen.com/websolar/login");
             driver.findElement(By.id("email")).sendKeys("super_admin@gmail.com");
             driver.findElement(By.id("password")).sendKeys("secret");
             driver.findElement(By.xpath("/html/body/div/div[2]/div/form/div[3]/div[2]/button")).click();
@@ -40,20 +47,21 @@ import org.openqa.selenium.chrome.ChromeOptions;
         }
         @Test
         public void test3() {
-            WebDriverManager.chromedriver().setup();
-            ChromeOptions options = new ChromeOptions();
-            options.addArguments("--remote-allow-origins=*");
-            WebDriver driver = new ChromeDriver(options);
-            driver.manage().window().maximize();
-            driver.get("http://solar.schoolnewgen.com/websolar/login");
+            //WebDriverManager.chromedriver().setup();
+            //ChromeOptions options = new ChromeOptions();
+            //options.addArguments("--remote-allow-origins=*");
+            //WebDriver driver = new ChromeDriver(options);
+            //driver.manage().window().maximize();
+            //driver.get("http://solar.schoolnewgen.com/websolar/login");
             driver.findElement(By.id("email")).sendKeys("super_admin@gmail.com");
             driver.findElement(By.id("password")).sendKeys("secret");
             driver.findElement(By.xpath("/html/body/div/div[2]/div/form/div[3]/div[2]/button")).click();
             driver.findElement(By.xpath("//*[@id=\"companiesModal\"]/div/div/div/form/button")).click();
             driver.findElement(By.xpath("/html/body/div[1]/aside/div/div[4]/div/div/nav/ul/li[3]/a")).click();
-            driver.findElement(By.xpath("/html/body/div[1]/aside/div/div[4]/div/div/nav/ul/li[3]/ul/li[2]")).click();
+            driver.findElement(By.xpath("/html/body/div[1]/aside/div/div[4]/div/div/nav/ul/li[3]/ul/li[2]/a")).click();
         }
     }
+
 
 
 
